13672fba016292f70013d8156a5c4d4aea980246
Arshin Jain <Arshin@Arshins-MacBook-Pro.local>
2017-06-17 16:10:05 -0700

    Add Component decorator and AppComponent Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,2 +1,17 @@
 import { Component } from '@angular/core';
 
+@Component({
+    selector: 'app-root',
+    template: '<h2>Hello World!</h2>',
+    styles: [
+        `
+        h2 {
+            font-family: sans;
+            font-size: 1.2em;
+        }
+        `
+    ]
+})
+export class AppComponent {
+
+}
