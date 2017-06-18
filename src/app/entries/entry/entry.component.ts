0e802b4f22a965bf7311f9e110a875e2d473ed02
Arshin Jain <arshin.jain@outlook.com>
2017-06-17 22:59:13 -0700

    Add Component Decor Params - Entry

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-    
+    selector: 'app-entry',
+    templateUrl: 'entry.component.html',
+    styleUrls: ['entry.component.css']
 })
 export class EntryComponent {
 
