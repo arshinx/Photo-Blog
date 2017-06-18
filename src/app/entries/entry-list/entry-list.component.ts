9f42da610df8a8729124e3b13bc5a58c0067ef45
Arshin Jain <arshin.jain@outlook.com>
2017-06-17 22:27:11 -0700

    Add Style to Component Decor

--- src/app/entries/entry-list/entry-list.component.ts
+++ src/app/entries/entry-list/entry-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-entry-list',
-    templateUrl: 'entry-list.component.html'
+    templateUrl: 'entry-list.component.html',
+    styleUrls: ['entry-list.component.css']
 })
 export class EntryListComponent {
 
