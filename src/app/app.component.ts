84898bd59ddab122d90d7e6c38411dd691bd8b0e
Arshin Jain <arshin.jain@outlook.com>
2017-06-17 18:52:25 -0700

    Add Type Extensions for Component Decorator Urls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component',
-    styleUrls: ['app.component']
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent {
 
