7f54480cee06a2a0374ad7b97e7977aa4dc930a9
Arshin Jain <arshin.jain@outlook.com>
2017-06-17 23:00:33 -0700

    Import Entry Comp. in App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { EntryListComponent } from './entries';
+import { EntryListComponent, EntryComponent } from './entries';
 
 @NgModule({
     imports: [BrowserModule],
