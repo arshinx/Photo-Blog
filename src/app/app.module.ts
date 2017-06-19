524ad630c665047107b4d8305d1b8aa25db70be3
Arshin Jain <arshin.jain@outlook.com>
2017-06-18 22:14:15 -0700

    App Module - Import Entry Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent } from './entries';
+import { EntryListComponent, EntryComponent, EntryService } from './entries';
 
 @NgModule({
     imports: [
