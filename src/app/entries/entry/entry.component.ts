7bb7346ad093265f2c4c5ca003f25d871a4cb1ac
Arshin Jain <arshin.jain@outlook.com>
2017-06-18 23:11:08 -0700

    entry property

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -7,12 +7,5 @@ import { Entry } from '../shared/entry.model';
     styleUrls: ['entry.component.css']
 })
 export class EntryComponent {
-    title: string = 'My First Photo';
-    photo: string = 'http://placehold.it/800x500?text=Angular Basics'
-    description: string = 'A Description of My First Photo';
-    comments: any[] = [
-        {name: "John", comment: "A Comment"},
-        {name: "Jim", comment: "A Comment"},
-        {name: "Jen", comment: "A Comment"}
-    ];
+    entry: Entry;
 }
