9f2bc9abfd49ac2516145cc75aab0d134bd980c1
Arshin Jain <arshin.jain@outlook.com>
2017-06-18 22:57:28 -0700

    Import o

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from '@angular/http';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppComponent } from './app.component';
 import { EntryListComponent, EntryComponent, EntryService } from './entries';
+import { InMemoryEntryService } from './backend';
 
 @NgModule({
     imports: [
