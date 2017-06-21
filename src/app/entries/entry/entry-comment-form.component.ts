ec83e9738dd78ae3a590dc460773b977bf04d72c
Arshin Jain <arshin.jain@outlook.com>
2017-06-20 23:33:33 -0700

    Entry Form Component - Error Fixes - Append '.html'

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-entry-comment-form',
-    templateUrl: 'entry-comment-form.component'
+    templateUrl: 'entry-comment-form.component.html'
 })
 
 export class EntryCommentFormComponent {
