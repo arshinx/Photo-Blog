671b781c0dbfcdff0dae1066f02f1f1def29c7ea
Arshin Jain <arshin.jain@outlook.com>
2017-06-20 20:57:26 -0700

    Entry Comment Form - Component Decor

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+@Component({
+    selector: 'app-entry-comment-form',
+    templateUrl: 'entry-comment-form.component'
+})
 class EntryCommentFormComponent {
-    
+
 }
