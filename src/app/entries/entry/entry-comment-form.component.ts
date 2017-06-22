507ee91ec501bda678c4c6de4398e435922bacd0
Arshin Jain <arshin.jain@outlook.com>
2017-06-22 01:01:44 -0700

    Entry Comment Form - Import Input

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { NgForm } from "@angular/forms";
 import { EntryService } from '../shared/entry.service';
 
